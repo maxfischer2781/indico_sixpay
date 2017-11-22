a0706deaec2f13e0c418dc93a3c84ae5fd79d5d1
Max Fischer <maxfischer2781@gmail.com>
2017-11-22 13:55:38 +0100

    added core of configuration and user request

---
+++ indico_sixpay/utility.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+
+from indico.util.i18n import make_bound_gettext
+
+#: internationalisation/localisation of strings
+gettext = make_bound_gettext('payment_sixpay')
