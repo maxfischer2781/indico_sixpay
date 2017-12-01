c6b84a05d1355c326972868ab94a1d8f2e39da26
Max Fischer <maxfischer2781@gmail.com>
2017-12-01 17:14:22 +0100

    buildscript now handles plantuml baking

--- build_docs.sh
+++ build_docs.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
+# Perform a complete build of the documentation
 set -e
 
 LIB_NAME=indico_sixpay
 DOCS_DIR=docs
 
 cd ${DOCS_DIR}
+# cleanup backed files
 touch source/api/dummy
 rm source/api/*
-# export SPHINX_APIDOC_OPTIONS=members,undoc-members,no-show-inheritance
+if which plantuml >/dev/null
+then
+    touch source/images/uml/dummy
+    rm source/images/uml/*
+    plantuml -tsvg -o ../images/uml/ source/uml/*.uml
+fi
+
+# sphinx build
 sphinx-apidoc --module-first --separate --output-dir=source/api ../${LIB_NAME} --force && \
 python2 $(which sphinx-build) -b html -d build/doctrees . build/html/ && \
 open build/html/index.html
