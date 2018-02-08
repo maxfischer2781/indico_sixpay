fdc660293c9dd3992c757af7417818fb72641030
Max Fischer <maxfischer2781@gmail.com>
2018-02-08 16:55:03 +0100

    repository badge

--- docs/index.rst
+++ docs/index.rst
@@ -13,6 +13,10 @@
     :alt: License
     :target: https://github.com/maxfischer2781/indico_sixpay/blob/master/LICENSE
 
+.. image:: https://img.shields.io/github/commits-since/maxfischer2781/indico_sixpay/v2.0.0.svg
+    :alt: Repository
+    :target: https://github.com/maxfischer2781/indico_sixpay/tree/master
+
 .. toctree::
     :maxdepth: 1
     :caption: Subtopics Overview:
--- indico_sixpay/__about__.py
+++ indico_sixpay/__about__.py
@@ -15,6 +15,10 @@
     :alt: License
     :target: https://github.com/maxfischer2781/indico_sixpay/blob/master/LICENSE
 
+.. image:: https://img.shields.io/github/commits-since/maxfischer2781/indico_sixpay/v2.0.0.svg
+    :alt: Repository
+    :target: https://github.com/maxfischer2781/indico_sixpay/tree/master
+
 Plugin for the Indico event management system to use EPayment via SIX Payment services.
 
 Quick Guide
